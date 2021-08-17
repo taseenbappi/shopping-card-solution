67b2367f129e9da5907e2267ef4f997affa7e2e4
taseenbappi <taseen.bappi@gmail.com>
2021-08-18 02:10:48 +0600

    shopping cart solution

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('I am in a separate file')
+// get product quantity for increment
+function getCount(isIncrement, inputCount, pdtPriceOf, forDisabled) {
+    const countInput = document.getElementById(inputCount);
+    const pdtPrice = document.getElementById('phone-price');
+    const countValueText = countInput.value;
+    const countValue = parseInt(countValueText);
+
+    if (isIncrement) {
+
+        let newCountValue = countValue + 1;
+        countInput.value = newCountValue;
+        pdtPrice.innerText = newCountValue * pdtPriceOf;
+        document.getElementById(forDisabled).removeAttribute('disabled');
+
+    }
+
+    else {
+
+        let newCountValue = countValue - 1;
+        if (newCountValue >= 1) {
+            countInput.value = newCountValue;
+            pdtPrice.innerText = newCountValue * pdtPriceOf;
+            document.getElementById(forDisabled).removeAttribute('disabled');
+        }
+        else {
+            document.getElementById(forDisabled).setAttribute('disabled', true);
+        }
+
+    }
+}
+
+// phone shopping
+document.getElementById('phone-increment-btn').addEventListener('click', function () {
+    getCount(true, 'phone-input', 1219, 'phone-increment-btn');
+});
+
+document.getElementById('phone-decrement-btn').addEventListener('click', function () {
+    getCount(false, 'phone-input', 1219, 'phone-increment-btn');
+
+});
+
+
